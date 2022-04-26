ed48030ddc471908f90585fd0986bd3cb1c62b80
Your Name <you@example.com>
2022-04-26 22:32:09 +0800

    Version_alpha

---
+++ script.js
@@ -0,0 +1,18 @@
+function changeImage()
+{
+    document.getElementById("home_img").src="activelogo.png"
+}
+function changeImageback()
+{
+    document.getElementById("home_img").src="logo.png"
+}
+function visible()
+{
+    x=document.getElementById("cover") 
+	x.style.visibility="visible"
+}
+function hide()
+{
+    x=document.getElementById("cover") 
+	x.style.visibility="hidden"
+}
