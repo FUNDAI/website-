a2b943d80ad28be19abd0d81b93dd715de49cb08
FUNDAI <96116846+FUNDAI@users.noreply.github.com>
2022-07-20 01:42:38 +0800

    we need HD pic

--- website--beta/jquery.js
+++ website--beta/jquery.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
     $("#home_logo").mouseenter(function(){
-        $(".logo").fadeTo("fast",0.5);
-        $(".about_frc").fadeTo("fast",0.5);
-        $(".about_us").fadeTo("fast",0.5);
-        $(".members").fadeTo("fast",0.5);
-        $(".contact_us").fadeTo("fast",0.5);
-        $(".changeeng").fadeTo("fast",0.5);
-        $(".space").fadeTo("fast",0.5);
-        $(".animated_7130").fadeTo("fast",0.5);
-        $(".logo").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%, 0.75) 0%,hsla(170, 65%, 40%, 0.75) 100%)");
-        $(".about_frc").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%, 0.75) 0%,hsla(170, 65%, 40%, 0.75) 100%)");
-        $(".about_us").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%, 0.75) 0%,hsla(170, 65%, 40%, 0.75) 100%)");
-        $(".members").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%, 0.75) 0%,hsla(170, 65%, 40%, 0.75) 100%)");
-        $(".contact_us").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%, 0.75) 0%,hsla(170, 65%, 40%, 0.75) 100%)");
-        $(".changeeng").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%, 0.75) 0%,hsla(170, 65%, 40%, 0.75) 100%)");
-        $(".space").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%, 0.75) 0%,hsla(170, 65%, 40%, 0.75) 100%)");
-        $(".animated_7130").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%, 0.75) 0%,hsla(170, 65%, 40%, 0.75) 100%)");
+        $(".logo").fadeTo("fast",0.8);
+        $(".about_frc").fadeTo("fast",0.8);
+        $(".about_us").fadeTo("fast",0.8);
+        $(".members").fadeTo("fast",0.8);
+        $(".contact_us").fadeTo("fast",0.8);
+        $(".changeeng").fadeTo("fast",0.8);
+        $(".space").fadeTo("fast",0.8);
+        $(".animated_7130").fadeTo("fast",0.8);
+        $(".logo").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%) 0%,hsla(170, 65%, 40%) 100%)");
+        $(".about_frc").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%) 0%,hsla(170, 65%, 40%) 100%)");
+        $(".about_us").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%) 0%,hsla(170, 65%, 40%) 100%)");
+        $(".members").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%) 0%,hsla(170, 65%, 40%) 100%)");
+        $(".contact_us").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%) 0%,hsla(170, 65%, 40%) 100%)");
+        $(".changeeng").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%) 0%,hsla(170, 65%, 40%) 100%)");
+        $(".space").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%) 0%,hsla(170, 65%, 40%) 100%)");
+        $(".animated_7130").css("background-image","linear-gradient(to bottom, hsla(170, 65%, 50%) 0%,hsla(170, 65%, 40%) 100%)");
         $(".logo").fadeTo("fast",1);
         $(".about_frc").fadeTo("fast",1);
         $(".about_us").fadeTo("fast",1);
@@ -26,14 +26,14 @@ $(document).ready(function(){
         $(".animated_7130").fadeTo("fast",1);
     })
     $("#home_logo").mouseleave(function(){
-        $(".logo").fadeTo("fast",0.5);
-        $(".about_frc").fadeTo("fast",0.5);
-        $(".about_us").fadeTo("fast",0.5);
-        $(".members").fadeTo("fast",0.5);
-        $(".contact_us").fadeTo("fast",0.5);
-        $(".changeeng").fadeTo("fast",0.5);
-        $(".space").fadeTo("fast",0.5);
-        $(".animated_7130").fadeTo("fast",0.5);
+        $(".logo").fadeTo("fast",0.8);
+        $(".about_frc").fadeTo("fast",0.8);
+        $(".about_us").fadeTo("fast",0.8);
+        $(".members").fadeTo("fast",0.8);
+        $(".contact_us").fadeTo("fast",0.8);
+        $(".changeeng").fadeTo("fast",0.8);
+        $(".space").fadeTo("fast",0.8);
+        $(".animated_7130").fadeTo("fast",0.8);
         $(".logo").css("background-image","linear-gradient(to bottom, hsl(193, 65%, 40%) 0%,hsl(193, 65%, 20%) 100%)");
         $(".about_frc").css("background-image","linear-gradient(to bottom, hsl(193, 65%, 40%) 0%,hsl(193, 65%, 20%) 100%)");
         $(".about_us").css("background-image","linear-gradient(to bottom, hsl(193, 65%, 40%) 0%,hsl(193, 65%, 20%) 100%)");
@@ -52,11 +52,11 @@ $(document).ready(function(){
         $(".animated_7130").fadeTo("fast",1);
     })
     $(".navtext").mouseenter(function(){
-        $(".cover").css("opacity","0.83");
+        $(".cover").css("opacity","0.9");
     })
     $(".navtext").mouseleave(function(){
         $(".menu_txt").mouseenter(function(){
-            $(".cover").css("opacity","0.83");
+            $(".cover").css("opacity","0.9");
         })
         $(".menu_txt").mouseleave(function(){
             $(".cover").css("opacity","0")
